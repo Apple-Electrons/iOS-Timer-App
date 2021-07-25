fe970857f94388ac4a4eb8b4132a5a1305d1259f
Apple-Electrons <87925354+Apple-Electrons@users.noreply.github.com>
2021-07-24 23:31:09 -0700

    Create SimplidoroApp.swift

---
+++ SimplidoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplidoroApp.swift
+//  Simplidoro
+//
+//  Created by Rodin Shokravi on 2021-07-24.
+//
+
+import SwiftUI
+
+@main
+struct SimplidoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
